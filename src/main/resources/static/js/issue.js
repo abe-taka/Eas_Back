015f426de0aedc27142401679da6b3a92d78795c
maru <ntiadori0821@gmail.com>
2021-12-06 22:34:14 +0900

    先生課題作成

--- src/main/resources/static/js/issue.js
+++ src/main/resources/static/js/issue.js
@@ -1,1 +1,15 @@
-/* 問題管理画面 */
+function openBtn (event){
+	const modalWindow = document.getElementById("modal_" + event);
+	const modalFilter = document.getElementById("filter");
+	
+	modalWindow.style.display = "block";
+	modalFilter.style.display = "block";
+}
+
+function closeBtn (event){
+	const modalWindow = document.getElementById("modal_" + event);
+	const modalFilter = document.getElementById("filter");
+	
+	modalWindow.style.display = "none";
+	modalFilter.style.display = "none";
+}
